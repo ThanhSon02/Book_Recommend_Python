f03d5acabbf4146ed9b4f104557374b7b768857e
dT0210 <duthanh41025@gmail.com>
2023-07-11 11:58:58 +0700

    add get highest rated books

--- server/main.py
+++ server/main.py
@@ -1,6 +1,7 @@
 from typing import Union
-
 from fastapi import FastAPI
+import numpy as np
+import pandas as pd
 
 app = FastAPI()
 
@@ -10,10 +11,17 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/books/highestrating")
+def getHighestRatedBooks():
+    books_df = pd.read_csv('Books.csv', dtype="string")
+    books_df = books_df.dropna()
+    ratings_df = pd.read_csv('Ratings.csv')
+    ratings_df = ratings_df.dropna()
 
+    ratings_mean = ratings_df.groupby("ISBN").agg({'Book-Rating': 'mean', 'User-ID': 'count'}).rename(columns={'User-ID': 'Rating-Count'})
+    books_df = pd.merge(books_df, ratings_mean, on='ISBN', how='left')
+    highestRated = books_df.where(books_df['Rating-Count'] > 10).dropna(subset=['ISBN']).sort_values('Book-Rating', ascending=False).head(10)
+    return highestRated
 
 @app.get("/books")
 def showBooks():
