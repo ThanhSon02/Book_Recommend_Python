3ef400c614ce0283941ec45c8789e14ee79c93d5
Thanh Sơn <106719620+ThanhSon02@users.noreply.github.com>
2023-07-14 15:11:57 +0700

    Update Details.jsx

--- client/src/pages/Details.jsx
+++ client/src/pages/Details.jsx
@@ -7,14 +7,26 @@ import uniqid from "uniqid";
 
 function Details() {
     const formRatingInitial = {
-        id: "",
-        name: "",
-        age: "",
-        rating: 0,
+        userId: "",
+        isbn: "",
+        bookRating: 0,
     };
 
-    const [data, setData] = useState({});
+    const initialData = {
+        isbn: "",
+        title: "",
+        author: "",
+        publishYear: 0,
+        publisher: "",
+        rating: "",
+        imgS: "",
+        imgM: "",
+        imgL: "",
+    };
+
+    const [data, setData] = useState(initialData);
     const [formRating, setFormRating] = useState(formRatingInitial);
+    const [ratingStatus, setRatingStatus] = useState(false);
 
     const location = useLocation();
     const id = location.state.id;
@@ -33,74 +45,49 @@ function Details() {
         e.preventDefault();
         console.log(formRating);
         axios
-            .post("http://127.0.0.1:8000/books/details/rating/", {
+            .post("http://127.0.0.1:8000/books/rate", {
                 data: formRating,
             })
             .then((res) => {
-                console.log(res.data);
+                setRatingStatus(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
             });
     };
 
     return (
         <>
             <Header />
-            <div className="w-screen flex flex-col items-center bg-[#213b64] text-white">
+            <div className="w-screen pb-8 flex flex-col items-center bg-[#213b64] text-white">
                 <div className="flex flex-col mt-4 ">
-                    <img src={data["Image-URL-L"]} />
+                    <img src={data.imgL} />
                     <h1 className="place-self-center font-semibold text-2xl">
-                        {data["Book-Title"]}
+                        {data.title}
                     </h1>
                     <div className="flex">
                         <h1 className="font-semibold mr-2">Author: </h1>
-                        <p>{data["Book-Author"]}</p>
+                        <p>{data.author}</p>
                     </div>
                     <div className="flex">
                         <h1 className="font-semibold mr-2">
                             Year Of Publication:
                         </h1>
-                        <p>{data["Year-Of-Publication"]}</p>
+                        <p>{data.publishYear}</p>
                     </div>
                     <div className="flex">
                         <h1 className="font-semibold mr-2">Publisher: </h1>
-                        <p>{data["Publisher"]}</p>
+                        <p>{data.publisher}</p>
+                    </div>
+                    <div className="flex">
+                        <h1 className="font-semibold mr-2">Book Rating: </h1>
+                        <p className="font-bold text-red-600">
+                            {parseFloat(data.rating).toFixed(1)}
+                        </p>
                     </div>
                 </div>
                 <form className="w-2/3 mt-5" onSubmit={handleSubmit}>
-                    <h1 className="text-xxl">Rating</h1>
                     <div className="mt-3 flex flex-col gap-2">
-                        <div className="flex items-center">
-                            <label className="mr-2">Your Name: </label>
-                            <input
-                                className="text-gray-800 px-2 py-3"
-                                type="text"
-                                placeholder="your name"
-                                value={formRating.name}
-                                onChange={(e) =>
-                                    setFormRating((prev) => ({
-                                        ...prev,
-                                        id: uniqid(),
-                                        name: e.target.value,
-                                    }))
-                                }
-                            />
-                        </div>
-                        <div className="flex items-center">
-                            <label className="mr-2">Age: </label>
-                            <input
-                                placeholder="age"
-                                className="text-gray-800 px-2 py-3"
-                                type="number"
-                                min={1}
-                                max={200}
-                                value={formRating.age}
-                                onChange={(e) =>
-                                    setFormRating((prev) => ({
-                                        ...prev,
-                                        age: e.target.value,
-                                    }))
-                                }
-                            />
-                        </div>
                         <div className="flex items-center">
                             <label className="mr-2">Rating: </label>
                             <Rating
@@ -109,7 +96,9 @@ function Details() {
                                 onChange={(e, value) =>
                                     setFormRating((prev) => ({
                                         ...prev,
-                                        rating: value,
+                                        userId: uniqid(),
+                                        isbn: id,
+                                        bookRating: value,
                                     }))
                                 }
                             />
@@ -119,6 +108,7 @@ function Details() {
                         Rate!
                     </button>
                 </form>
+                {ratingStatus ? <h1>{ratingStatus}</h1> : <></>}
             </div>
         </>
     );
